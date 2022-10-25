d63bcf8f4d30026d1179da2b2cc3d65df1534f18
psmacabulos <patrick.macabulos@gmail.com>
2022-10-25 10:46:10 +0800

    Improved script to make resolution adjustable.  - grid size to be adjusted using prompt.  - added a button to change resolution.  - improved UI.

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
 const container = document.querySelector('.container');
 
-let gridNumber = 16 * 16;
+/**
+ *
+ * @param {*} flexVar
+ * @param {*} gridNumber
+ *
+ * Create a Function to create a default 16 x 16 grid.
+ */
+
+const createGrid = (flexVar = 6.25, gridNumber = 16 * 16) => {
+  container.innerHTML = '';
+  for (let i = 0; i < gridNumber; i++) {
+    let div = document.createElement('div');
+    div.classList.add('grid');
+    div.style = `flex: 1 1 ${flexVar}%;`;
+    // Add hover effect to each div
+    div.addEventListener('mouseover', function () {
+      this.style.backgroundColor = '#B1AFFF';
+    });
+    container.appendChild(div);
+  }
+};
+
+createGrid();
 
 /**
- * Create the 16 x 16 grid. By appending div to the
- * container element.
+ * Add listener to button changing the resolution of the grid.
  */
-for (let i = 0; i < gridNumber; i++) {
-  let div = document.createElement('div');
-  div.classList.add('grid');
-  // Add hover effect to each div
-  div.addEventListener('mouseover', function () {
-    this.style.backgroundColor = 'green';
-  });
-  container.appendChild(div);
-}
+const resolution = document.querySelector('button');
+
+resolution.addEventListener('click', () => {
+  let width = +prompt('What resolution do you want? Choose from 1 - 100');
+  if (width === undefined || width === null || width === 0 || width > 100) {
+    createGrid();
+  } else {
+    let gridNumber = width ** 2;
+    let flexVar = (1 / width) * 100;
+    createGrid(flexVar, gridNumber);
+  }
+});
